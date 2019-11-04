2ba4305d135313e2410c1e7e7b1170c1f33fa1cc
Davidodari <davidkibzodari@gmail.com>
2019-11-04 12:33:33 +0300

    add view extensions

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 Library of things am tired of repeating on Android
 
+Feel free to contribute by adding your own extensions
 
-## In Progress...
+## In Progress... 
--- android-extensions/src/main/java/com/odari/android/extensions/DisplayUtils.kt
+++ android-extensions/src/main/java/com/odari/android/extensions/DisplayUtils.kt
@@ -1,6 +1,7 @@
 package com.odari.android.extensions
 
 import android.content.Context
+import android.view.View
 import android.widget.Toast
 
 fun Context.displayShortToast(data: Any) {
@@ -9,4 +10,12 @@ fun Context.displayShortToast(data: Any) {
 
 fun Context.displayLongToast(data: Any) {
     Toast.makeText(this, "$data", Toast.LENGTH_LONG).show()
+}
+
+fun View.hide() {
+    visibility = View.GONE
+}
+
+fun View.show() {
+    visibility = View.VISIBLE
 }
